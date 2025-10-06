b1a0ad8008ecec33bbd8867ec219a197be9ec983
unknown <oncec672@gmail.com>
2025-10-06 17:53:42 -0500

    Creacio de los modulos de KairosFit

---
+++ src/common/entities/estado.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('estados')
+export class Estado {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    tipo: string;
+}
